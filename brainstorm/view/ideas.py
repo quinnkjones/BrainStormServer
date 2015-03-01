37b2539130c215372bb8ae58db4d562da364dc14
quinnkjones <qjones1@mix.wvu.edu>
2015-02-28 19:46:44 -0500

    not sure why I have to put this one up

--- brainstorm/view/ideas.py
+++ brainstorm/view/ideas.py
@@ -1,7 +1,28 @@
 from distill.renderers import renderer
+import speech_recognition as sr
+from sql.orm import Transcription
+
+
+def recognize(source,mid):
+	r = sr.Recognizer()
+	audio = r.record(source) # extract audio data from the file
+	
+	try:
+		list = r.recognize(audio,True) # generate a list of possible transcriptions
+		Session().add(Transcription(mid,list[0]["text"]))
+		try:
+			Session().commit()
+		except:
+			Session().rollback()
+		
+	except LookupError: # speech is unintelligible
+		
 
 
 class IdeaController(object):
     @renderer('prettyjson')
     def get_ideas(self, request, resposne):
         return request.user.ideas
+ 
+	
+
--- brainstorm/voiceutils.py
+++ brainstorm/voiceutils.py
@@ -1,21 +1,27 @@
 import speech_recognition as sr
+
 r = sr.Recognizer()
-print r.energy_threshold
-with sr.WavFile("./test.wav") as source: # use "test.wav" as the audio source
-	audio = r.record(source) # extract audio data from the file
-	
+
+def recognize(recognizer,audio):
+	print recognizer.energy_threshold
 	try:
 		list = r.recognize(audio,True) # generate a list of possible transcriptions
 		print("Possible transcriptions:")
 		for prediction in list:
-			print(" " + prediction["text"] + " (" + str(prediction["confidence"]*100) + "%)")
+			print prediction
+			
+			
 	except LookupError: # speech is unintelligible
-		print("Could not understand audio")
+		print "didn't do the thing"
+		
+
+print r.energy_threshold
+for i in range(2,6):
+	with sr.WavFile("../test"+str(i)+".wav") as source: # use "test.wav" as the audio source
+		print "./test"+str(i)+".wav"
+		audio = r.record(source) # extract audio data from the file
+		recognize(r,audio)
 
 
-def recognize(recognizer,audio):
-	list = r.recognize(audio,True) # generate a list of possible transcriptions
-		print("Possible transcriptions:")
-		for prediction in list:
-			print(" " + prediction["text"] + " (" + str(prediction["confidence"]*100) + "%)")
-	
+
+
